501657ddfaae0c625fa932da2657a26c721cf723
Pham Van Hung <phamvanhung242005@gmail.com>
2026-07-02 15:24:58 +0700

    [FEAT]:APACHE KAFKA, AVRO

---
+++ notification-service/src/main/java/com/example/notification_service/service/NotificationService.java
@@ -0,0 +1,47 @@
+package com.example.notification_service.service;
+
+import com.example.order_service.event.OrderPlacedEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent){
+        log.info("Got Message from order-placed topic {}", orderPlacedEvent);
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springshop@email.com");
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
+            messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully",
+                    orderPlacedEvent.getOrderNumber()));
+            messageHelper.setText(String.format("""
+                            Hi
+
+                            Your order with order number %s is now placed successfully.
+
+                            Best Regards
+                            Spring Shop
+                            """,
+                    orderPlacedEvent.getOrderNumber()));
+        };
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order Notifcation email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new RuntimeException("Exception occurred when sending mail to springshop@email.com", e);
+        }
+    }
+}
--- order-service/src/main/java/com/example/order_service/service/OrderService.java
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -1,23 +1,27 @@
 package com.example.order_service.service;
-
-
+import com.example.order_service.event.OrderPlacedEvent;
 import com.example.order_service.client.InventoryServiceClient;
 import com.example.order_service.dto.OrderRequest;
 import com.example.order_service.model.Order;
 import com.example.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryServiceClient inventoryServiceClientClientClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest){
-        var isProductInStock = inventoryServiceClientClientClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        var isProductInStock = inventoryServiceClientClientClient.isInStock(orderRequest.skuCode(),
+                orderRequest.quantity());
 
         if(isProductInStock){
             //map orderrequest to order object
@@ -30,6 +34,22 @@ public class OrderService {
 
             // save order to orderrepository
             orderRepository.save(order);
+
+            //send the message to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+
+            System.out.println(orderRequest.userDetails().email());
+            System.out.println(orderRequest.userDetails().firstName());
+            System.out.println(orderRequest.userDetails().lastName());
+
+            log.info("Start - Sending Order Placed Event {} to Kafka topic order-placed",  orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending Order Placed Event {} to Kafka topic order-placed", orderPlacedEvent);
+
         } else {
             throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
         }
